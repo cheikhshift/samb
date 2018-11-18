66c4463a9c074614cb06999aedd52e988873e67f
CheikhSeck <jvmstix@gmail.com>
2018-11-18 20:12:23 +0000

    import provided variables from other routes.

--- project_funcs.go
+++ project_funcs.go
@@ -40,8 +40,10 @@ func (p *Project) MergeWith(file *Project) {
 	p.Packages = append(p.Packages, file.Packages...)
 	p.Import = append(p.Import, file.Import...)
 
-	// process file route imports
+
 	p.Templates.Template = append(p.Templates.Template, file.Templates.Template...)
+
+	p.Routes.Provide = append(p.Routes.Provide, file.Routes.Provide...)
 	p.Routes.Route = append(p.Routes.Route, file.Routes.Route...)
 
 }
