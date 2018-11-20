e716f456a9a99e10b915d095bbfad720bb024091
CheikhSeck <jvmstix@gmail.com>
2018-11-20 20:38:12 +0000

    add godoc badge.

--- samb_test.go
+++ samb_test.go
@@ -67,40 +67,39 @@ func TestProcessImports(t *testing.T) {
 
 }
 
-
-func TestMergeWith(t *testing.T){
+func TestMergeWith(t *testing.T) {
 
 	var mergeTests = []struct {
 		expectedImportLength int
-		expectedRouteLength int
-		project *Project
+		expectedRouteLength  int
+		project              *Project
 	}{
-			{
-				3, 0 , &Project{
-				Import : []string{"Foo", "Baz",""},
-				},
+		{
+			3, 0, &Project{
+				Import: []string{"Foo", "Baz", ""},
 			},
-			{
-				2, 1 , &Project{
-				Import : []string{"Foo", "Baz"},
-				Routes : Routes{
-						Route : []Route{
-							Route{Path : "Sample"},
-						},
+		},
+		{
+			2, 1, &Project{
+				Import: []string{"Foo", "Baz"},
+				Routes: Routes{
+					Route: []Route{
+						Route{Path: "Sample"},
 					},
 				},
 			},
-			{
-				4, 2 , &Project{
-				Import : []string{"Foo", "Baz", "Extra", "TEst"},
-				Routes : Routes{
-						Route : []Route{
-							Route{Path : "Sample"},
-							Route{Path : "SampleThree"},
-						},
+		},
+		{
+			4, 2, &Project{
+				Import: []string{"Foo", "Baz", "Extra", "TEst"},
+				Routes: Routes{
+					Route: []Route{
+						Route{Path: "Sample"},
+						Route{Path: "SampleThree"},
 					},
 				},
 			},
+		},
 	}
 
 	for i, tt := range mergeTests {
@@ -110,11 +109,11 @@ func TestMergeWith(t *testing.T){
 
 			tempProject.MergeWith(tt.project)
 
-			if len(tempProject.Import) != tt.expectedImportLength  {
+			if len(tempProject.Import) != tt.expectedImportLength {
 				t.Errorf("got %v, want %v for number of imports", len(tempProject.Import), tt.expectedImportLength)
 			}
 
-			if len(tempProject.Routes.Route) != tt.expectedRouteLength  {
+			if len(tempProject.Routes.Route) != tt.expectedRouteLength {
 				t.Errorf("got %v, want %v for number of routes", len(tempProject.Routes.Route), tt.expectedRouteLength)
 			}
 		})
@@ -122,15 +121,15 @@ func TestMergeWith(t *testing.T){
 
 }
 
-func TestHasProvider(t *testing.T){
+func TestHasProvider(t *testing.T) {
 
 	testProject := &Project{
-		Provider : []Global{
-			Global{Name:"Foo"},
-			Global{Name:"Baz"},
-			Global{Name:"Go"},
-			Global{Name:"Vn"},
-			Global{Name:"Bar"},
+		Provider: []Global{
+			Global{Name: "Foo"},
+			Global{Name: "Baz"},
+			Global{Name: "Go"},
+			Global{Name: "Vn"},
+			Global{Name: "Bar"},
 		},
 	}
 
@@ -146,37 +145,35 @@ func TestHasProvider(t *testing.T){
 	for _, name := range providerTests {
 		t.Run(name, func(t *testing.T) {
 
-	
 			has := testProject.HasProvider(name)
 
-			if  !has  {
-				t.Errorf("got %v, want %v for provider " + name, has, true)
+			if !has {
+				t.Errorf("got %v, want %v for provider "+name, has, true)
 			}
 
-			
 		})
 	}
 
 }
 
-func TestGetProvider(t *testing.T){
+func TestGetProvider(t *testing.T) {
 
 	testProject := &Project{
-		Provider : []Global{
-			Global{Name:"Foo", Type:"int"},
-			Global{Name:"Baz", Type:"string"},
-			Global{Name:"Go", Type:"int64"},
-			Global{Name:"Vn", Type: "int" },
-			Global{Name:"Bar", Type: "string"},
+		Provider: []Global{
+			Global{Name: "Foo", Type: "int"},
+			Global{Name: "Baz", Type: "string"},
+			Global{Name: "Go", Type: "int64"},
+			Global{Name: "Vn", Type: "int"},
+			Global{Name: "Bar", Type: "string"},
 		},
 	}
 
 	// name of providers to look for.
-	expectedProviderTypes := []struct{
+	expectedProviderTypes := []struct {
 		expectedType string
-		name string
+		name         string
 	}{
-		{"int", "Foo" },
+		{"int", "Foo"},
 		{"string", "Baz"},
 		{"int64", "Go"},
 		{"int", "Vn"},
@@ -186,14 +183,12 @@ func TestGetProvider(t *testing.T){
 	for _, tt := range expectedProviderTypes {
 		t.Run(tt.name, func(t *testing.T) {
 
-	
 			p := testProject.GetProvider(tt.name)
 
-			if  p.Type != tt.expectedType  {
-				t.Errorf("got type %v, want %v for provider " + p.Name, p.Type , tt.expectedType )
+			if p.Type != tt.expectedType {
+				t.Errorf("got type %v, want %v for provider "+p.Name, p.Type, tt.expectedType)
 			}
 
-			
 		})
 	}
 
