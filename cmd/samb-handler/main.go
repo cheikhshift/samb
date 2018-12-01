6c390cc69d183aac120010703b91dcdf508721c5
CheikhSeck <jvmstix@gmail.com>
2018-12-01 10:58:45 +0000

    Add SAMB handler tool.

---
+++ cmd/samb-handler/main.go
@@ -0,0 +1,112 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"os/exec"
+	"strings"
+)
+
+func main() {
+
+	name := flag.String("name", "FooBar", "Name of Handler to add.")
+	project := flag.String("project", "./", "Path to project")
+
+	v := flag.String("verb", "*", "HTTP verb of handler. Leave blank to generate a route handler.")
+
+	flag.Parse()
+
+	os.Chdir(*project)
+
+	// set path to where the handler source will be saved
+	savePath := "./pkg/api/" + *name + "_handler.go"
+
+	var router string
+
+	if *v == "*" {
+
+		router = GenerateRouter(*name)
+
+		for _, v := range verbs {
+
+			router += GenerateHandler(*name, v)
+
+		}
+
+	}
+
+	if *v != "*" {
+
+		router = "package api\n\n\n"
+
+		router += GenerateHandler(*name, *v)
+
+	}
+
+	fmt.Println("Saving file to : ", savePath)
+
+	err := ioutil.WriteFile(savePath, []byte(router), 0700)
+
+	if err != nil {
+		panic(err)
+	}
+
+	manageImports()
+
+}
+
+func GenerateHandler(name, v string) (result string) {
+	return fmt.Sprintf(
+		handlerTemplate,
+		name,
+		v,
+		name,
+		v+strings.Title(name),
+	)
+}
+
+func GenerateRouter(name string) (result string) {
+	fileSkeleton := []string{
+		name,
+		name,
+		name,
+		name,
+	}
+
+	for _, v := range verbs {
+
+		fileSkeleton = append(fileSkeleton, v+strings.Title(name))
+	}
+
+	fileSkeleton = append(fileSkeleton, name)
+
+	result = fmt.Sprintf(
+		routeTemplate,
+		fileSkeleton[0],
+		fileSkeleton[1],
+		fileSkeleton[2],
+		fileSkeleton[3],
+		fileSkeleton[4],
+
+		fileSkeleton[5],
+		fileSkeleton[6],
+		fileSkeleton[7],
+		fileSkeleton[8],
+		fileSkeleton[9],
+	)
+
+	return
+
+}
+
+func manageImports() {
+	cmd := exec.Command("goimports", "-w", "./")
+
+	err := cmd.Run()
+
+	if err != nil {
+		panic(err)
+	}
+}
---
+++ cmd/samb-handler/template.go
@@ -0,0 +1,49 @@
+package main
+
+var verbs = []string{
+	"Put",
+	"Patch",
+	"Delete",
+	"Post",
+	"Get",
+}
+
+var routeTemplate = `package api
+
+
+var %sNotFound = "{\"error\" : \"Not supported\", \"code\" : 405 }"
+
+// Handle%s Routes PUT,PATCH,DELETE,POST and GET
+// requests for corresponding
+// handler %s.
+func Handle%s(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodPut:
+		%s(w, r)
+	case http.MethodPatch:
+		%s(w, r)
+	case http.MethodDelete:
+		%s(w, r)
+	case http.MethodPost:
+		%s(w, r)
+	case http.MethodGet:
+		%s(w, r)
+	default:
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		w.Write([]byte(%sNotFound))
+	}
+}
+
+`
+
+var handlerTemplate = `// %s handles %s HTTP requests
+// for %s
+func %s(w http.ResponseWriter, r *http.Request){
+
+	w.Write([]byte("Please implement me."))
+
+}
+
+
+`
